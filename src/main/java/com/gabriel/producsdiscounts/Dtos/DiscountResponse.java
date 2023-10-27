5f3b98af786829281972429beb614aaba95f3c79
gabriel <pgabrieldeveloper@outlook.com>
2023-10-27 12:59:31 -0300

    initial commit

---
+++ src/main/java/com/gabriel/producsdiscounts/Dtos/DiscountResponse.java
@@ -0,0 +1,4 @@
+package com.gabriel.producsdiscounts.Dtos;
+
+public class DiscountResponse {
+}
---
+++ src/main/java/com/gabriel/producsdiscounts/ProducsDiscountsApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.producsdiscounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducsDiscountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducsDiscountsApplication.class, args);
+	}
+
+}
