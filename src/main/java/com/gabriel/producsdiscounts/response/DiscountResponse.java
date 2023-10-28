586752f2ed113caf0de9e4d6b2775d44f7e43ffc
gabriel <pgabrieldeveloper@outlook.com>
2023-10-28 09:14:24 -0300

    refactor(response): ajustes responses

--- src/main/java/com/gabriel/producsdiscounts/response/DiscountResponse.java
+++ src/main/java/com/gabriel/producsdiscounts/response/DiscountResponse.java
@@ -2,12 +2,14 @@ package com.gabriel.producsdiscounts.response;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Builder;
+import lombok.Getter;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @Builder
+@Getter
 public class DiscountResponse {
 
     private String id;
     private String cupom;
-    private String desconto;
+    private float desconto;
 }
--- src/main/java/com/gabriel/producsdiscounts/response/ProductResponse.java
+++ src/main/java/com/gabriel/producsdiscounts/response/ProductResponse.java
@@ -1,12 +1,14 @@
 package com.gabriel.producsdiscounts.response;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Builder;
+import lombok.Getter;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @Builder
+@Getter
 public class ProductResponse {
     private String id;
     private String name;
-    private String price;
+    private float price;
 }
 
