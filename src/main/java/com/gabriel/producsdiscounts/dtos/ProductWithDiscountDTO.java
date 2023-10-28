4aba3adeef1078b417905c57e4c86a03eae966e6
gabriel <pgabrieldeveloper@outlook.com>
2023-10-28 09:14:56 -0300

    feat(dto) create dto response

--- src/main/java/com/gabriel/producsdiscounts/dtos/ProductWithDiscountDTO.java
+++ src/main/java/com/gabriel/producsdiscounts/dtos/ProductWithDiscountDTO.java
@@ -1,4 +1,18 @@
 package com.gabriel.producsdiscounts.dtos;
 
-public class ProductWithDiscount {
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@JsonSerialize
+@Getter
+public class ProductWithDiscountDTO {
+    private String productId;
+    private String productName;
+    private float originValue;
+    private float currentValue;
+    private float discountValue;
+    private String cupom;
 }
+
